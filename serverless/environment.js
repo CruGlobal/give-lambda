1723ed20aac443a2f4a952d2dc0cddca7c1352d5
Bill Randall <william.randall@cru.org>
2021-08-16 15:45:34 -0400

    Set up serverless

---
+++ serverless/environment.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = () => {
+  // Use dotenv to load local development overrides
+  require('dotenv').config()
+  return {
+    ENVIRONMENT: process.env['ENVIRONMENT'] || 'development'
+  }
+}
